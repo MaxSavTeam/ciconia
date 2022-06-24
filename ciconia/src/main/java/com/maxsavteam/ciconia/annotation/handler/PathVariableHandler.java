d321039946ba7ee9524943e47d31d1969a3b962b
Max Savitsky <maxsavitsky2015@gmail.com>
2022-06-24 14:01:29 +0300

    chore: add exception throwing when path variable not found

--- ciconia/src/main/java/com/maxsavteam/ciconia/annotation/handler/PathVariableHandler.java
+++ ciconia/src/main/java/com/maxsavteam/ciconia/annotation/handler/PathVariableHandler.java
@@ -3,6 +3,7 @@ package com.maxsavteam.ciconia.annotation.handler;
 import com.maxsavteam.ciconia.annotation.PathVariable;
 import com.maxsavteam.ciconia.converter.Converter;
 import com.maxsavteam.ciconia.exception.IncompatibleClassException;
+import com.maxsavteam.ciconia.exception.InvalidPathVariableException;
 
 import java.lang.annotation.Annotation;
 import java.util.Optional;
@@ -18,6 +19,15 @@ public class PathVariableHandler implements ParameterAnnotationHandler {
 	public Optional<Object> handle(Annotation annotation, Class<?> parameterType, Context context) {
 		PathVariable pathVariable = (PathVariable) annotation;
 		String variableName = pathVariable.value();
+		if(!context.getPathVariables().containsKey(variableName)) {
+			throw new InvalidPathVariableException(
+					String.format(
+							"Path variable \"%s\" is not declared in method path (%s)",
+							variableName,
+							context.getMethodPath()
+					)
+			);
+		}
 		String variableValue = context.getPathVariables().get(variableName);
 		Optional<Object> op = Converter.convertToParameterType(variableValue, parameterType);
 		if(op.isPresent())
