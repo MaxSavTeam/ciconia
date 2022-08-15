124303eab1fd8baba11c1f6c15037595a85d0d10
Max Savitsky <maxsavitsky2015@gmail.com>
2022-08-15 15:36:16 +0300

    fix: remove support for configuration inner classes

--- ciconia/src/main/java/com/maxsavteam/ciconia/processor/ConfigurerProcessor.java
+++ ciconia/src/main/java/com/maxsavteam/ciconia/processor/ConfigurerProcessor.java
@@ -31,8 +31,8 @@ public class ConfigurerProcessor {
 		if (Modifier.isAbstract(classModifiers))
 			throw new InvalidConfigurationDeclarationException("Configuration cannot be abstract class");
 
-		if (!checkIfClassCanBeAccessedAsStatic(cl))
-			throw new InvalidConfigurationDeclarationException("Configuration should be static or top-level public class");
+		if (!Modifier.isPublic(classModifiers) || cl.getDeclaringClass() != null)
+			throw new InvalidConfigurationDeclarationException("Configuration should be public top-level class");
 
 		for (Method method : cl.getDeclaredMethods()) {
 			if (!method.isAnnotationPresent(ObjectFactory.class))
@@ -53,39 +53,6 @@ public class ConfigurerProcessor {
 		}
 
 		return new Configurer(cl, methods);
-	}
-
-	/**
-	 * Returns true if class is top-level class or can be accessed as static.<br>
-	 * For example
-	 * <pre>{@code
-	 * class Test1 {
-	 *      static class Test2 {
-	 *          static class Test3 {
-	 *
-	 *          }
-	 *      }
-	 * }
-	 * }</pre>
-	 * class Test3 can be accessed, but now cannot, because Test2 is not static or top-level
-	 * <pre>{@code
-	 * 	class Test1 {
-	 *      class Test2 {
-	 *          static class Test3 {
-	 *
-	 *          }
-	 *      }
-	 * }
-	 * }</pre>
-	 */
-	private boolean checkIfClassCanBeAccessedAsStatic(Class<?> cl) {
-		int modifiers = cl.getModifiers();
-		if(!Modifier.isPublic(modifiers))
-			return false;
-		Class<?> declaringClass = cl.getDeclaringClass();
-		if (declaringClass == null)
-			return true;
-		return Modifier.isStatic(modifiers) && checkIfClassCanBeAccessedAsStatic(declaringClass);
 	}
 
 }
